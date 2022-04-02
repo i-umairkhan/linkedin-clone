150365fc82aef45502dbd5632a25192f1aee1285
Umair Khan <umair.k2607@gmail.com>
2022-04-03 02:54:42 +0500

    Input Section Completed

---
+++ src/Feed.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import './Feed.css'
+import CreateIcon from '@mui/icons-material/Create';
+import InputOption from './InputOption';
+import ImageIcon from '@mui/icons-material/Image';
+import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
+import EventNoteIcon from '@mui/icons-material/EventNote';
+import CalendarViewDayIcon from '@mui/icons-material/CalendarViewDay';
+
+const Feed = () => {
+  return (
+    <div className='feed'>
+        <div className='feed__inputContainer'>
+            <div className='feed__input'>
+            <CreateIcon />
+            <form>
+                <input type='text'/>
+                <button type='submit'>Send</button>
+            </form>
+            </div>
+            <div className='feed__inputOptions'>
+                <InputOption Icon={ImageIcon} title='Photo' color='#70B5F9'/>
+                <InputOption Icon={SubscriptionsIcon} title='Vedio' color='#E7A33E'/>
+                <InputOption Icon={EventNoteIcon} title='Event' color='#C0CBCD'/>
+                <InputOption Icon={CalendarViewDayIcon} title='write artical' color='#71CF5E'/>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Feed
